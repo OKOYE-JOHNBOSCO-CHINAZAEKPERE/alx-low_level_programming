3dcd274ce233ed92abd7c20429f4e0905d44def1
OKOYE-JOHNBOSCO-CHINAZAEKPERE <tranquil3438@gmail.com>
2022-09-08 07:05:48 -0700

    My first C programming commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,22 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: always 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
