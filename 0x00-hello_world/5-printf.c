28428664eca6cf983c715dec67dbbea3f5f44935
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-09-08 16:33:08 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: always 0
- *
- *     */
+/*
+     * main - Entry point
+     *
+     * Return: always 0
+     */
 
 int main(void)
 
