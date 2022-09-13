e6d042ce9bb9f20557552adb1760941b6954e4b0
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-09-13 23:42:28 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,31 +1,44 @@
-#include "main.h"
-#include "6-abs.c"
 #include <stdio.h>
-
+/**
+* print_to_98 - function
+*
+* @n: parameter
+*
+* Return: natural numbers
+*/
 void print_to_98(int n)
 {
-	int i;
+	int upper = 98;
 
-	if (n > 98)
+	if (n > upper)
 	{
-		for (i = n; i >= 98; i--)
+		while (n >= upper)
 		{
-			printf("%d", i);
-			if (i != 98)
+			if (n != upper)
 			{
-				printf(", ");
+				printf("%d%s", n, ", ");
 			}
-		}
-	} else
-	{
-		for (i = n; i <= 98; i++)
-		{
-			printf("%d", i);
-			if (i != 98)
+			else
 			{
-				printf(", ");
+				printf("%d", n);
 			}
+			n--;
 		}
 	}
-	printf("\n");
+	else
+	{
+		while (n <= upper)
+		{
+			if (n != upper)
+			{
+				printf("%d%s", n, ", ");
+			}
+			else
+			{
+				printf("%d", n);
+			}
+			n++;
+		}
+	}
+	putchar(10);
 }
