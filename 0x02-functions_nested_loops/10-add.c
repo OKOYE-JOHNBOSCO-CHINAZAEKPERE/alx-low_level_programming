74032972e0c030a7712316687913fd7e3b27eae4
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-09-13 18:52:50 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,12 @@
-hkghkjhjhghgdffckhjfghfd
+#include "main.h"
+/**
+ * add - adding it up
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of both integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
