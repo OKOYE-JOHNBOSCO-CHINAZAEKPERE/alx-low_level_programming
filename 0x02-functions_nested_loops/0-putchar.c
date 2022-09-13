ea325bbadeeac62686cd70988171fe4561a0d9ca
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-09-13 17:23:56 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,24 @@
-jhdsgjyhghydgdhdc
-gfbrkj
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
+int main(void)
+{
+	_putchar('B');
+	_putchar('E');
+	_putchar('S');
+	_putchar('T');
+	_putchar(' ');
+	_putchar('S');
+	_putchar('c');
+	_putchar('h');
+	_putchar('o');
+  _putchar('o');
+  _putchar('l');
+	_putchar('\n');
+	return (0);
+}
