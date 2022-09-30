df6b5ad71d94871605fa5ad9fafcb0a662be49cc
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-09-30 20:53:02 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,1 +1,28 @@
-ffgjjthj
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program entry
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	mul = 1;
+	if (argc > 2)
+	{
+		for (i = 1; i < argc; i++)
+			mul *= atoi(argv[i]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
