ff536ebc5fd4f0f5b7fed215dd2409c708a4ad82
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-09-30 20:49:48 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,17 @@
-hbvbgbbvcbgv
+#include <stdio.h>
+
+/**
+ * main - program entry
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
