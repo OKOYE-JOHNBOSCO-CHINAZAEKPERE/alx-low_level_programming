2e925baedc260a567c12c44262f2724cf59d78e9
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-09-30 20:52:26 +0100

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,17 @@
-ghfygfgf
+#include <stdio.h>
+
+/**
+ * main - program entry
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
