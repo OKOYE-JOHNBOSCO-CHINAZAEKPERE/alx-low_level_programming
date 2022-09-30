f6317d74c5ac04d00ce7e292f8e66ca85f619944
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-09-30 20:50:26 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,15 @@
-ggfhgg
+#include <stdio.h>
+
+
+/**
+ * main - program entry
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
