cc4ad1fcdbd1ef52a09fd2284f972164fa6b7cc9
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-10-07 02:27:57 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,1 +1,37 @@
-hgfhgfgy
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - program entry
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	mul = 1;
+	if (argc == 3)
+	{
+		if (isdigit(*argv[1]) && isdigit(*argv[2]))
+		{
+			for (i = 1; i < argc; i++)
+				mul *= atoi(argv[i]);
+			printf("%d\n", mul);
+		}
+		else
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (0);
+}
