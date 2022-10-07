8186ba2a6ba98329e16a96fd0ce859af4600a06c
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-10-07 02:12:57 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,16 @@
-ghghgg
+#include "main.h"
+
+/**
+* malloc_checked - allocates memory using malloc
+* @b: bytes allocate
+* Return: pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
