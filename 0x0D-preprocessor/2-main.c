e81e370163bde18b09a160d63158d8a28eaf4d10
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-10-11 00:05:58 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,14 @@
-hggjhgnbn
+nes (11 sloc)  137 Bytes
+
+#include <stdio.h>
+
+/**
+ * main - program entry
+ *
+ * Return: ALways zero
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
