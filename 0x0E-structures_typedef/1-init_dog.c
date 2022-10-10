447778d7b3e608c588247deb5f64b2c043e2a666
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-10-11 00:21:09 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,1 +1,22 @@
-kjhjhh
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ *
+ * @d: pointer variable of type struct dog
+ * @name: pointer to name
+ * @age: age
+ * @owner: pointer to owner
+ *
+ * Return: Always 0.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
