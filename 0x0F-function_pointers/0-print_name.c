354801ccc54944ff892030ed64d42abc53b8ae23
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-10-12 19:09:41 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,19 @@
-hgjhgjhg
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name
+ *
+ * @name: pointer to name array
+ * @f: function pointer of type char
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	void (*ptr)(char *);
+
+	if (name && f)
+	{
+		ptr = f;
+		ptr(name);
+	}
+}
