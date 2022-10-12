fe66ab3918bb2cb36ba9d2c513903d1de5401997
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-10-12 19:11:11 +0100

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,1 +1,22 @@
-nhbvhhgjx
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a
+ * parameter on each element of an array
+ *
+ * @array: pointer to array fo functions
+ * @size: size of array
+ * @action: function to execute
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	void (*ptr)(int);
+
+	ptr = action;
+	if (array && action)
+		for (i = 0; i < size; i++)
+			ptr(array[i]);
+}
