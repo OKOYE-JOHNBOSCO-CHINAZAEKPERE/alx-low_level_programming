b7888ec3a40fcf80678f13b8a6e998d2400a7619
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-10-12 19:22:07 +0100

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,15 @@
-hfghfghf
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stddef.h>
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+void print_name(char *name, void (*f)(char *));
+void print_elem(int elem);
+void print_elem_hex(int elem);
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int is_strictly_positive(int elem);
+int abs_is_98(int elem);
+int is_98(int elem);
+
+#endif /* FUNCTION_POINTERS_H */
