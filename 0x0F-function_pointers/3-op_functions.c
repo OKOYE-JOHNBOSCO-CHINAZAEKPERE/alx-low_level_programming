18df5b4a25202c4ce1342ac71150d000f7dd5755
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-10-12 19:15:15 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,1 +1,61 @@
-njbnbnbmnbj
+#include "3-calc.h"
+/**
+ * op_add - addition operation
+ * @a: integer a
+ * @b: integer b
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtraction operation
+ * @a: integer a
+ * @b: integer b
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - subtraction operation
+ * @a: integer a
+ * @b: integer b
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division operation
+ * @a: integer a
+ * @b: integer b
+ * Return: division of a and b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - subtraction operation
+ * @a: integer a
+ * @b: integer b
+ * Return: modulus of a and b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
