1102e6589a04c15b844ffb4ca4580c1df6284756
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-10-25 00:11:28 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,21 @@
-jhjhgjh
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ *
+ * @h: linked list
+ *
+ * Return: Number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned long int i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (i);
+}
