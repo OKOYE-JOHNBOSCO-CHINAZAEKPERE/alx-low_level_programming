2eb17ea7dfeffd963645b5ef8b4b39a2cbf8b71e
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-10-25 00:20:03 +0100

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,25 @@
-hghghg
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ *
+ * @head: address to linked list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp1;
+	listint_t *temp2;
+
+	if (head != NULL)
+	{
+		temp1 = *head;
+		while ((temp2 = temp1) != NULL)
+		{
+			temp1 = temp1->next;
+			free(temp2);
+		}
+		*head = NULL;
+	}
+}
