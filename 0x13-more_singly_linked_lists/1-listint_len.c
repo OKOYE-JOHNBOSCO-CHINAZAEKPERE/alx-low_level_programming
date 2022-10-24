29b9b1e7239310797d97c0228cafb751a67557d8
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-10-25 00:11:58 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,18 @@
-hjghggghg
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ *
+ * @h: linked list
+ *
+ * Return: Number of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned long int i;
+
+	for (i = 0; h != NULL; i++)
+		h = h->next;
+
+	return (i);
+}
