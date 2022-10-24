aa27ae30de4c3dfd57839ef6a5d9726a846939ce
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-10-25 00:22:57 +0100

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,1 +1,23 @@
-gvfvhhbhbv
+#include "lists.h"
+
+/**
+ * sum_listint - sums all data (n) of a listint_t linked list
+ *
+ * @head: address to linked list
+ *
+ * Return: returns sum or 0 if linked list is empty
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	unsigned int i;
+
+	if (head == NULL)
+		return (0);
+	for (i = 0; head != NULL; i++)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
