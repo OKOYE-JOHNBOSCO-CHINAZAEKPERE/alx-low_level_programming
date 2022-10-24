06b9cd27be2b2a685f3323b536088b53bf3faf2b
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-10-25 00:20:46 +0100

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,1 +1,25 @@
-hvhvvhvv
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked list
+ *
+ * @head: address to linked list
+ *
+ * Return: returns the head node’s data (n)
+ */
+int pop_listint(listint_t **head)
+{
+	int head_node;
+	listint_t *temp1;
+	listint_t *temp2;
+
+	if (*head == NULL)
+		return (0);
+
+	temp1 = *head;
+	head_node = temp1->n;
+	temp2 = temp1->next;
+	free(temp1);
+	*head = temp2;
+	return (head_node);
+}
