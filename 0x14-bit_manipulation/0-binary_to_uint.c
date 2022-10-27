62431514dd16c45b3cc64064080dc6b44850f5f8
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-10-27 23:36:37 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,34 @@
-hjghghg
+#include "main.h"
+
+/**
+ * binary_to_uint - coverts a binary to an unsigned int
+ * @b: pointer to the strings of 0 & 1 chars
+ * Return: the converted number, or 0 if;
+ * there is one or more chars in the string b that is not 0 or 1,
+ * b is NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int j, base;
+	unsigned int val;
+
+	val = 0;
+
+	if (!b)
+		return (0);
+
+	for (j = 0; b[j] != '\0'; j++)
+		;
+
+	for (j--, base = 1; j >= 0; j--, base *= 2)
+	{
+		if (b[j] != '0' && b[j] != '1')
+			return (0);
+
+		if (b[j] & 1)
+			val += base;
+	}
+
+	return (val);
+}
