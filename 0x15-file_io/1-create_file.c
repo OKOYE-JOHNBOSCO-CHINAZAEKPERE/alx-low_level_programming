e097a55129737510b09d2002554cc64eada80442
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-10-31 23:32:00 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,38 @@
-hbghggfgfg
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file
+ *
+ * @filename: name of file to be created
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, fd, txt;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (i = 0; text_content[i]; i++)
+		;
+
+	txt = write(fd, text_content, i);
+
+	if (txt == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
