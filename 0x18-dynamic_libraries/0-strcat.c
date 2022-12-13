3a553f2a6d53b0a3565295c067fd566389cf4bf4
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-12-13 02:20:11 +0100

    Update 0-strcat.c

--- 0x18-dynamic_libraries/0-strcat.c
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -1,1 +1,23 @@
-k
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ *
+ * @dest: pointer to string to be appended to
+ * @src: pointer to string appended
+ *
+ * Return: pointer to resulting string @dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; *(dest + i) != '\0'; i++)
+		;
+	for (j = 0; *(src + j) != '\0'; j++)
+	{
+		*(dest + i) = *(src + j);
+		i++;
+	}
+	return (dest);
+}
