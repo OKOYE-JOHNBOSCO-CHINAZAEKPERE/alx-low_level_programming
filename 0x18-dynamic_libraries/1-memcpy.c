a61b31a146af8056797ba567cc9b6949094fd0a6
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-12-13 02:22:25 +0100

    Update 1-memcpy.c

--- 0x18-dynamic_libraries/1-memcpy.c
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -1,1 +1,20 @@
-g
+#include "main.h"
+/**
+ * _memcpy - copies n bytes from the memory area src to memory area dest
+ *
+ * @src: pointer to memory area
+ * @dest: memory area destination
+ * @n: number of byte(s) to be filled
+ *
+ * Return: a pointer to the memory area dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
