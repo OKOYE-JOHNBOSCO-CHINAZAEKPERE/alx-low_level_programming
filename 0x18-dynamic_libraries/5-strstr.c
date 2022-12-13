0155b687c2d80e042ed73ee1dfd861c98e0b5544
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-12-13 02:36:05 +0100

    Update 5-strstr.c

--- 0x18-dynamic_libraries/5-strstr.c
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -1,1 +1,31 @@
-p
+#include "main.h"
+/**
+ * _strstr - finds first occurrence of the substring needle
+ * in the string haystack
+ * @haystack: string to be searched
+ * @needle: set of byte(s) to be searched for
+ *
+ * Return: pointer to byte s that matches one of the bytes in accept
+ * or NULL if no such byte is found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *bhaystack;
+	char *pneedle;
+
+	while (*haystack != '\0')
+	{
+		bhaystack = haystack;
+		pneedle = needle;
+
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+		{
+			haystack++;
+			pneedle++;
+		}
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
+	}
+	return (0);
+}
