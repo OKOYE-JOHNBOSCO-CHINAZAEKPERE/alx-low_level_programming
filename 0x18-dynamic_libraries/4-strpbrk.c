efec08f206e7aececf7f2222a25ed316e9ecda66
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-12-13 02:35:07 +0100

    Update 4-strpbrk.c

--- 0x18-dynamic_libraries/4-strpbrk.c
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -1,1 +1,24 @@
-j
+#include "main.h"
+/**
+ * _strpbrk - gets the length of a prefix of substring
+ *
+ * @s: string to be searched
+ * @accept: set of byte(s) to be searched for
+ *
+ * Return: pointer to byte s that matches one of the bytes in accept
+ * or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+				return (s + i);
+		}
+	}
+	return ('\0');
+}
