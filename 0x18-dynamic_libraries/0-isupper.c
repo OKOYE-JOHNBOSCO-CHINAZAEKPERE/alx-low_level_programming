45f42285dd8a96d062ce3d46e416211ca0b6b7c3
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-12-13 02:18:25 +0100

    Update 0-isupper.c

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,1 +1,19 @@
-p
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: integer c
+ *
+ * Return: returns 1 if c is uppercase, returns 0 otherwise
+ */
+int _isupper(int c)
+{
+	int val;
+
+	if (c >= 'A' && c <= 'Z')
+		val = 1;
+	else
+		val = 0;
+	return (val);
+}
