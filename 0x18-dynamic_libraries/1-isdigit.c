5a1302d5327be927899e7468df904cf295eaef32
OKOYE JOHNBOSCO CHINAZAEKPERE <111225867+OKOYE-JOHNBOSCO-CHINAZAEKPERE@users.noreply.github.com>
2022-12-13 02:21:36 +0100

    Update 1-isdigit.c

--- 0x18-dynamic_libraries/1-isdigit.c
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -1,1 +1,19 @@
-po
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit 0 through 9
+ *
+ * @c: integer c
+ *
+ * Return: returns 1 if c is a digit, returns 0 otherwise
+ */
+int _isdigit(int c)
+{
+	int val;
+
+	if (c >= '0' && c <= '9')
+		val = 1;
+	else
+		val = 0;
+	return (val);
+}
